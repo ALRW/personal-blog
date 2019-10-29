b59c49199a5919c84ddfca3fc0bd0cda8a2aa072
ALRW <awerner1@googlemail.com>
2019-10-29 15:39:25 +0000

    [AW] Inital Commit

---
+++ components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="columns">
+    <div class="column is-10 is-offset-1">
+      <nav class="navbar has-shadow">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">Andrew | Werner </a>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
