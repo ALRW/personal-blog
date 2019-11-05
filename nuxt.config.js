634513593650b79d2bcb077088ac55713774b68b
ALRW <awerner1@googlemail.com>
2019-11-05 14:29:59 +0000

    [AW] added disqus comments to blog posts routing info still required

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/disqus'],
   /*
    ** Nuxt.js dev-modules
    */
--- pages/_post/index.vue
+++ pages/_post/index.vue
@@ -5,13 +5,21 @@
         <div class="content">
           <div v-html="post.html"></div>
         </div>
+        <hr />
+        <div class="comments">
+          <disqus shortname="andrew-werner"></disqus>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
+import Disqus from 'vue-disqus/src/vue-disqus.vue'
 export default {
+  components: {
+    Disqus
+  },
   async asyncData({ params }) {
     const post = await import(`@/articles/${params.post}.md`)
     return {
