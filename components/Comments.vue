203d276700cc06a6e9fd2c129eafe56cb8ddeb7d
ALRW <awerner1@googlemail.com>
2019-11-05 16:05:34 +0000

    [AW] added show-hide logic to comments section

--- components/Comments.vue
+++ components/Comments.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="comments">
+    <button
+      class="button is-fullwidth comment-button has-text-weight-semibold"
+      @click="show = !show"
+    >
+      {{ show ? `Hide Comments` : `Show Comments` }}
+    </button>
     <disqus
+      v-if="show"
       :shortname="shortname"
       :identifier="identifier"
       :url="url"
@@ -31,9 +38,22 @@ export default {
   },
   data() {
     return {
+      show: false,
       shortname: `andrew-werner`,
       url: this.$route.query.page
     }
   }
 }
 </script>
+
+<style>
+.comments {
+  border-top: 2px solid whitesmoke;
+  border-bottom: 2px solid whitesmoke;
+  padding-top: 1em;
+  padding-bottom: 1em;
+}
+.comment-button {
+  margin: 1em 0 1em 0;
+}
+</style>
