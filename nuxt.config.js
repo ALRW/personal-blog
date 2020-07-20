2d8436b74a1526d6c27cfd209e20a1359ab850b1
ALRW <awerner1@googlemail.com>
2020-07-20 17:03:45 +0100

    [AW] udpated to extract all css into single file

--- nuxt.config.js
+++ nuxt.config.js
@@ -107,6 +107,18 @@ export default {
   },
   build: {
     extractCSS: true,
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.(css|vue)$/,
+            chunks: 'all',
+            enforce: true
+          }
+        }
+      }
+    },
     postcss: {
       preset: {
         features: {
